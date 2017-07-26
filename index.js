31fb36cf59b0b2f9a52940376a3716c465b1afe3
Doug Hughes <doug@doughughes.net>
2017-07-26 08:23:25 -0400

    this is my prepwork before class

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 const mustacheExpress = require('mustache-express');
 const moment = require('moment');
 const foods = require('./food');
 const fs = require('fs');
 const app = express();
+
+// Set app to use bodyParser() middleware.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//'expressValidator' must come after 'bodyParser', since data must be parsed first!
+app.use(expressValidator());
 
 // tell express to use mustache
 app.engine('mustache', mustacheExpress());
@@ -26,6 +35,29 @@ app.get('/food/:id', function(req, res) {
   res.render('food', {
     foodItem: foods[req.params.id]
   });
+});
+
+// show the form for creating a new food item
+app.get('/foodForm', function(req, res) {
+  res.render('foodForm');
+});
+
+// create a new food item when a form is posted
+app.post('/createFood', function(req, res) {
+  // check that the name is valid
+  req.checkBody('name', 'You must enter a name for your food!').notEmpty();
+
+  // check for errors
+  var errors = req.validationErrors();
+
+  if (errors) {
+    // Render validation error messages
+    var html = errors;
+    res.send(html);
+  } else {
+    var name = req.body.name;
+    res.send('you created ' + name);
+  }
 });
 
 // make express listen on port 3000
